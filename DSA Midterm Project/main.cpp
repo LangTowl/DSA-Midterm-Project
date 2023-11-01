4e2bc34e2ad7f9e9432e2936712a21f69ea902bb
Lang <119524847+LangTowl@users.noreply.github.com>
2023-11-01 15:38:35 -0400

    Add files via upload

--- DSA Midterm Project/main.cpp
+++ DSA Midterm Project/main.cpp
@@ -1,10 +1,19 @@
 /* Library Inclusion Start */
 #include <iostream>
+#include <cstring>
 #include <fstream>
 #include <string>
+
+using namespace std;
 /* Library Inclusion End */
 
-/* Class Declaration Start */
+/* Global Variables Start */
+int tree_root_count;
+int object_count;
+int queery_count;
+/* Global Variables End */
+
+/* Class Declarations Start */
 class itemNode {
 
 public:
@@ -19,7 +28,7 @@ public:
         right = NULL;
     }
 
-    itemNode(const char itemName[], const int population) {
+    itemNode(char itemName[], int population) {
         std::strcpy(name, itemName);
         count = population;
         left = NULL;
@@ -28,6 +37,7 @@ public:
 };
 
 class treeNameNode {
+private:
 
 public:
     char treeName[50];
@@ -41,138 +51,85 @@ public:
         right = NULL;
     }
 
-    treeNameNode(const char name[]) {
+    treeNameNode(char name[]) {
         std::strcpy(treeName, name);
         theTree = NULL;
         left = NULL;
         right = NULL;
     }
 };
-/* Class Declaration End */
+/* Class Declarations End */
 
-/* Build BST Functions Start*/
-treeNameNode* add_tree_nodes(int treeNameCount) {
-    // in.txt accessor
-    std::ifstream in;
-    in.open("in.txt");
-
-    treeNameNode* head = nullptr;
-
-    // Checks to see if in.txt open succesfully
-    if (in.is_open() == true) {
-        // for loop to iterate through contents of in.txt based on item quantities
-        for(int i = 0; i <= treeNameCount; i++) {
-            /* String collection start */
-            std::string txt;
-            std::getline(in, txt);
-
-            char buffer[60];
-            std::strcpy(buffer, txt.c_str());
-
-            char substring_one[20];
-            std::sscanf(buffer, "%s", substring_one);
-            /* String collection start */
-
-            treeNameNode* new_node = new treeNameNode(substring_one);
-
-            // Conditional to skip first line of text file
-            if (i != 0) {
-                if (head == nullptr) {
-                    head = new_node;
-                } else {
-                    if (head->left == nullptr && head->right == nullptr) {
-                        treeNameNode* current = head;
-                            while (current->left != nullptr) {
-                                current = current->left;
-                            }
-                        current->left = new_node;
-                    } else if (head->left != nullptr && head->right == nullptr) {
-                        treeNameNode* current = head;
-                            while (current->right != nullptr) {
-                                current = current->left;
-                            }
-                        current->right = new_node;
-                    } else if (head->left != nullptr && head->right != nullptr) {
-                        treeNameNode* current = head;
-                            while (current->left != nullptr) {
-                                current = current->left;
-                            }
-                        current->left = new_node;
-                    }
-                }
-            }
-        }
-    } else {
-        // Output errer message should the file fail to open
-        std::cerr << "Error: Failed to open in.txt" << std::endl;
+/* BST builder functions start */
+treeNameNode* insert_root_node(treeNameNode* node, char name[20]) {
+    if(node == nullptr){
+      return new treeNameNode(name);
     }
 
-    in.close();
-    return head;
+    /* Alphabetical comparison start */
+    int comparison_result = strcmp(name, node->treeName);
+
+    if (comparison_result < 0) {
+        node->left = insert_root_node(node->left, name);
+    } else if (comparison_result > 0) {
+        node->right = insert_root_node(node->right, name);
+    }
+    /* Alphabetical comparison start */
+
+    return node;
 }
 
-void add_items_to_tree(int treeNameCount, int itemCount, treeNameNode* tree) {
-    // in.txt accessor
-    std::ifstream in;
-    in.open("in.txt");
+treeNameNode* add_roots_to_tree(ifstream& input) {
 
-    // Checks to see if in.txt open succesfully
-    if (in.is_open() == true) {
-        // for loop to iterate through contents of in.txt based on item quantities
-        for(int i = 0; i <= treeNameCount + itemCount + 1; i++) {
-            /* String collection start */
-            std::string txt;
-            std::getline(in, txt);
+    treeNameNode* root = nullptr;
 
-            char buffer[60];
-            std::strcpy(buffer, txt.c_str());
+    for (int i = 0; i <= tree_root_count + 1; i++) {
+        if (i != 0) {
+            string name;
+            char name_char[20];
 
-            char substring_one[20];
-            char substring_two[20];
-            char substring_three[20];
-            std::sscanf(buffer, "%s %s %s", substring_one, substring_two, substring_three);
-            /* String collection start */
-
-            if (i != 0 && i > treeNameCount + 1 && i < treeNameCount + itemCount + 1) {
-                // process_item_addition(tree, substring_one, substring_two, std::stoi(substring_three));
-            }
+            getline(input, name, '\n');
+            strcpy(name_char, name.c_str());
+            
+            root = insert_root_node(root, name_char);
         }
     }
 
-    in.close();
+    return root;
 }
 
-void process_item_addition(treeNameNode* tree, std::string type, std::string item, int data) {
+/* BST builder functions end */
 
+/* Traversal functions start */
+void inorder_traversal(treeNameNode* root) {
+    if (root != nullptr) {
+        inorder_traversal(root->left);
+        std::cout << root->treeName << endl;
+        inorder_traversal(root->right);
+    }
 }
-/* Build BST Functions Start*/
+/* Traversal functions end */
 
 int main() {
-    /* Determin Item Quantities Start */
-    int treeNameCount = 0;
-    int itemCount = 0;
-    int queryCount = 0;
+    /* in.txt accessor start */
+    ifstream input;
+    input.open("in.txt");
+    input >> tree_root_count >> object_count >> queery_count;
+    /* in.txt accessor end */
 
-    // Update variable quantities
-    FILE *collect = fopen("in.txt", "r");
-    fscanf(collect, "%d %d %d", &treeNameCount, &itemCount, &queryCount);
-    /* Determin Item Quantities End */
+    /* Code to run upon successful file access start */
+    if (input.is_open() == true) {
+        treeNameNode* root = add_roots_to_tree(input);
 
-    treeNameNode* head = add_tree_nodes(treeNameCount);
-    add_items_to_tree(treeNameCount, itemCount, head);
-
-    treeNameNode* left = head;
-    while (left != nullptr) {
-        std::cout << left->treeName << std::endl;
-        left = left->left;
+        inorder_traversal(root);
+    } 
+    /* Code to run upon successful file access end */
+    else 
+    /* Code to run upon failed file access start */
+    {
+        cerr << "Error: Failed to open [in.txt]" << endl;
     }
+    /* Code to run upon failed file access end */
 
-    std::cout << std::endl;
-
-    treeNameNode* right = head;
-    while (right != nullptr) {
-        std::cout << right->treeName << std::endl;
-        right = right->right;
-    }
     return 0;
 }
